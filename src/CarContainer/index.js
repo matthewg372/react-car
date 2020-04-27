1a0eacb7a6cd6919b2156994c899669388de9c2a
matthewg372 <59273107+matthewg372@users.noreply.github.com>
2020-04-27 18:04:12 -0500

    getting api files

--- src/CarContainer/index.js
+++ src/CarContainer/index.js
@@ -2,6 +2,34 @@ import React from 'react'
 
 
 class CarContainer extends React.Component{
+	constructor(){
+		super()
+		this.state={
+			cars: []
+		}
+	}
+	componentDidMount(){
+		this.getCars()
+	}
+
+	getCars = async () => {
+		try{
+			const url = process.env.REACT_APP_API_URL + '/api/v1/cars/'
+			const carsResponse = await fetch(url)
+			const carsJson = await carsResponse.json()
+			console.log(carsJson.data);
+			this.setState({
+				cars: carsJson.data
+			})
+		
+		}catch(err){
+			console.log(err)	
+		}
+		
+	}
+
+
+
 	render(){
 		return(
 			<h1>connected</h1>
